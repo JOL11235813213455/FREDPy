40ad0313042a357b504193c8f4365ee54bd39b49
JOL11235813213455 <31505803+JOL11235813213455@users.noreply.github.com>
2017-09-05 11:27:43 -0500

    Update pull.py
    Removed API key

--- pull.py
+++ pull.py
@@ -3,7 +3,7 @@ import pandas as pd
 from string import ascii_uppercase
 
 
-fred = Fred(api_key='47df1dd2cde5f239d39b2e750c64de83')
+fred = Fred(api_key='')
 ip = fred.get_series('GDPA')
 
 print ip
